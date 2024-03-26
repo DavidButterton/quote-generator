1297fa21b325248fb58d37efd7a5d404db297f41
DavidButterton <sadhu.butterton@gmail.com>
2024-03-25 18:00:38 -0700

    added js async function, random generate quote function, variables for grabbing html elements and adding text content to them'

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("testing");
+const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+apiQuotes = []
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const quoteAuthor = document.getElementById('author');
+const twittterButton = document.getElementById('twitter');
+const newQuoteButton = document.getElementById('new-quote')
+console.log(newQuoteButton);
+
+function generateQuote() {
+    const newQuote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    quoteText.textContent = newQuote.text;
+    quoteAuthor.textContent = newQuote.author;
+    console.log(newQuote)
+}
+
+async function getQuotes() {
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        generateQuote();
+        console.log(apiQuotes)
+    } catch(error) {
+        //hanlde error message
+    }
+}
+
+
+//on load
+getQuotes();
+
+// //another option using quotes inside the quote.js file with the local quotes variable
+// function newQuoteLocal() {
+//     const localQuote = localQuotes[Math.floor(Math.random() * localQuotes.length)]
+//     console.log(localQuote)
+// }
+
+// newQuoteLocal();
