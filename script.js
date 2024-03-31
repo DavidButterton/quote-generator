0642edf12ab9a9d8e9d0df2354753168d6cec9fc
DavidButterton <sadhu.butterton@gmail.com>
2024-03-31 11:01:44 -0700

    learned how to add a loader with a div element, using css styling to shape and animate, then using JavaScript with the hidden property to show loading animation happening when loading a quote and hid the animation while showing the quote when loading is complete

--- script.js
+++ script.js
@@ -1,46 +1,61 @@
-const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
-apiQuotes = []
-const quoteContainer = document.getElementById('quote-container');
-const quoteText = document.getElementById('quote');
-const quoteAuthor = document.getElementById('author');
-const twittterButton = document.getElementById('twitter');
-const newQuoteButton = document.getElementById('new-quote')
+const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+apiQuotes = [];
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const quoteAuthor = document.getElementById("author");
+const twittterButton = document.getElementById("twitter");
+const newQuoteButton = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 console.log(newQuoteButton);
 
+//show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//hide loading
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
 
 function generateQuote() {
-    const newQuote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    //check if author field is blank and replace it with 'unknown'
-    if(!newQuote.author) {
-        quoteAuthor.textContent = "unknown"
-    } else {
-        quoteAuthor.textContent = newQuote.author;
-    }
-    //check the quote length to determine styling
-    if(newQuote.text.length > 50) {
-        quoteText.classList.add('long-quote');
-    } else {
-        quoteText.classList.remove('long-quote');
-    }
-    quoteText.textContent = newQuote.text;
-    
-    console.log(newQuote)
+    loading();
+  const newQuote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  //check if author field is blank and replace it with 'unknown'
+  if (!newQuote.author) {
+    quoteAuthor.textContent = "unknown";
+  } else {
+    quoteAuthor.textContent = newQuote.author;
+  }
+  //check the quote length to determine styling
+  if (newQuote.text.length > 50) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = newQuote.text;
+//set Quote, hide Loader
+complete();
+  console.log(newQuote);
 }
 
 async function getQuotes() {
-    try {
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
-        generateQuote();
-        console.log(apiQuotes)
-    } catch(error) {
-        //hanlde error message
-    }
+    loading();
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    generateQuote();
+    console.log(apiQuotes);
+  } catch (error) {
+    //hanlde error message
+  }
 }
-
 
 //on load
 getQuotes();
+
 
 // //another option using quotes inside the quote.js file with the local quotes variable
 // function newQuoteLocal() {
@@ -52,9 +67,9 @@ getQuotes();
 
 //to tweet a quote
 function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`;
-    window.open(twitterUrl, '_blank');
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`;
+  window.open(twitterUrl, "_blank");
 }
 
-twittterButton.addEventListener('click', tweetQuote);
-newQuoteButton.addEventListener('click', generateQuote);
+twittterButton.addEventListener("click", tweetQuote);
+newQuoteButton.addEventListener("click", generateQuote);
